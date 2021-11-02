3a5b94bb9c544381adf1b2a340d70ba729ca71dc
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-11-02 12:05:45 +0530

    track songs

---
+++ src/components/track/track-card.component.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { withRouter } from 'react-router'
+
+import './track-card.styles.css'
+
+const TrackCard = ({ name, color, img, history }) => {
+  const handleClick = () => {
+    const fetchURL = 'https://shazam.p.rapidapi.com/search'
+    const fetchParams = { term: name, locale: 'en-US', limit: '20' }
+    const fetchHost = 'shazam.p.rapidapi.com'
+
+    history.push({
+      pathname: `/${name}/playlist`,
+      state: {
+        urlParams: fetchParams,
+        url: fetchURL,
+        host: fetchHost,
+        song: true,
+      },
+    })
+  }
+
+  return (
+    <div
+      className="muxic__track__card-container"
+      style={{ backgroundColor: color }}
+      onClick={handleClick}
+    >
+      <div className="muxic__track__text-block">{name}</div>
+      <div className="muxic__track__image-block">
+        <div className="muxic__track__image">
+          <img src={img} alt="telgu" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default withRouter(TrackCard)
