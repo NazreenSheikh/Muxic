8ab0c01b1f2e1b178390473df35758f6c14b90f4
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-11-02 11:43:22 +0530

    Welcome comp with styles

---
+++ src/components/welcome/welcome.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import { Link } from 'react-router-dom'
+import './welcome.styles.css'
+
+const Welcome = () => {
+  return (
+    <div className="welcome-wrapper">
+      <div className="welcome-logo">MUSICA</div>
+      <p className="welcome-subtitle">Music is the shorthand of emotion</p>
+    </div>
+  )
+}
+
+export default Welcome
