57e044b51ba1d86822b9791e59164339df8b1472
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-11-02 11:57:53 +0530

    muxic genres

---
+++ src/components/genre/genre.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './genre.styles.css'
+
+const Genre = ({
+  genre: { name, urlPath },
+  colors: { code, color, bgColor },
+}) => {
+  const fetchURL = 'https://shazam-core.p.rapidapi.com/v1/charts/genre-world'
+  const fetchParams = { genre_code: code.toUpperCase(), limit: '20' }
+  const fetchHost = 'shazam-core.p.rapidapi.com'
+
+  return (
+    <Link
+      to={{
+        pathname: `/${code}/playlist`,
+        state: {
+          urlParams: fetchParams,
+          url: fetchURL,
+          host: fetchHost,
+        },
+      }}
+    >
+      <div
+        className="muxic__genre-container"
+        style={{ backgroundColor: bgColor }}
+      >
+        <div
+          className="muxic__genre-container__text"
+          style={{ background: color }}
+        >
+          <p>{name}</p>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default Genre
