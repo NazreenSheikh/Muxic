e15ed4d060064fdc6fc8972418b599c2be1d2f39
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-11-02 11:56:39 +0530

    renderd comp to page

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Welcome from './components/welcome/welcome.component'
+import SearchPage from './pages/search/search-page.component'
+import DisplayPage from './pages/display/display.component'
+import PlaylistPage from './pages/platlist/playlist.component'
+
+import './App.css'
+import LyricsPage from './pages/lyrics/lyrics.component'
+import PlaySong from './components/playsong/playsong.component'
+import { PlayProvider } from './context/songContext'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <PlayProvider>
+          <SearchPage />
+          <Welcome />
+          <Switch>
+            <Route exact path="/" component={DisplayPage} />
+            <Route exact path="/:key/playlist" component={PlaylistPage} />
+            <Route exact path="/:song/lyrics" component={LyricsPage} />
+          </Switch>
+          <PlaySong />
+        </PlayProvider>
+      </Router>
+    </div>
+  )
+}
+
+export default App
