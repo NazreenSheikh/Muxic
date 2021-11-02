6dbc383930e9c93c1d40dac2212c9f8593322d85
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-11-02 12:07:30 +0530

    musica to Muxic

--- src/components/welcome/welcome.component.jsx
+++ src/components/welcome/welcome.component.jsx
@@ -5,7 +5,7 @@ import './welcome.styles.css'
 const Welcome = () => {
   return (
     <div className="welcome-wrapper">
-      <div className="welcome-logo">MUSICA</div>
+      <div className="welcome-logo">MUXIC</div>
       <p className="welcome-subtitle">Music is the shorthand of emotion</p>
     </div>
   )
