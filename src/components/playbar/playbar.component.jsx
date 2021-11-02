42bc942696d40abb6af0028fac9774bb07a4bf32
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-11-02 12:03:06 +0530

    muxic playbar and svgs

---
+++ src/components/playbar/playbar.component.jsx
@@ -0,0 +1,95 @@
+import React, { useContext } from 'react'
+import { AiFillPauseCircle, AiFillPlayCircle } from 'react-icons/ai'
+import { withRouter } from 'react-router'
+import { BsMusicNoteList } from 'react-icons/bs'
+import {
+  HideContext,
+  HideDispatchContext,
+  PlayContext,
+  PlayDispatchContext,
+} from '../../context/songContext'
+
+import './playbar.styles.css'
+
+const PlayBar = ({
+  playNo,
+  title,
+  subtitle,
+  img,
+  audio,
+  togglePlay,
+  playPause,
+  history,
+}) => {
+  const playDetails = useContext(PlayContext)
+  const setPlayDetails = useContext(PlayDispatchContext)
+  const hide = useContext(HideContext)
+  const setHide = useContext(HideDispatchContext)
+
+  const handleClick = () => {
+    history.push({
+      pathname: `/${title}/lyrics`,
+      state: {
+        title: title,
+        artist: subtitle,
+        cover: img,
+        audio: audio,
+      },
+    })
+  }
+
+  const handlePlayDetails = (playStatus) => {
+    setPlayDetails({
+      title: title,
+      subtitle: subtitle,
+      image: img,
+      audio: audio,
+      status: playStatus,
+    })
+  }
+
+  return (
+    <div className="muxic__song-playbar__container">
+      <div className="muxic_song-playbar__container__details">
+        <div className="muxic_song-playbar__container__playNo">
+          <p>{playNo}.</p>
+        </div>
+        <div className="muxic_song-playbar__container__img-div">
+          <img src={img} alt={title} />
+        </div>
+        <div className="muxic_song-playbar__container__desc-div">
+          <h5>{title}</h5>
+          <p>{subtitle}</p>
+        </div>
+      </div>
+      <div className="muxic_song-playbar__container__btn-div">
+        {!playDetails.status || !playPause[title + subtitle] ? (
+          <AiFillPlayCircle
+            fontSize={28}
+            onClick={() => {
+              if (hide) setHide(false)
+              handlePlayDetails(!playPause[title + subtitle])
+              togglePlay(title + subtitle)
+            }}
+          />
+        ) : (
+          <AiFillPauseCircle
+            fontSize={28}
+            onClick={() => {
+              setPlayDetails((prev) => ({
+                ...prev,
+                status: false,
+              }))
+              togglePlay(title + subtitle)
+            }}
+          />
+        )}
+        <div className="muxic_song-playbar__container__btn-div_lyrics">
+          <BsMusicNoteList fontSize={13} onClick={handleClick} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default withRouter(PlayBar)
