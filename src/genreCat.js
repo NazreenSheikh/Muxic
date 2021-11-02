14032342ede6b5b6a6f3806c190e3677cfc77b8a
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-11-02 11:46:17 +0530

    bg and data

---
+++ src/genreCat.js
@@ -0,0 +1,76 @@
+const genreCat = [
+    {
+        code: 'pop',
+        bgColor: '#d7f8ff',
+    },
+    {
+        code: 'HIP_HOP_RAP',
+        bgColor: '#FCE6F0',
+    },
+    {
+        code: 'dance',
+        bgColor: '#FFEAB9',
+    },
+    {
+        code: 'electronic',
+        bgColor: '#FDDFD8',
+    },
+    {
+        code: 'soul_rnb',
+        bgColor: '#bfffd3',
+    },
+    {
+        code: 'alternative',
+        bgColor: '#d7f8ff',
+    },
+    {
+        code: 'rock',
+        bgColor: '#FCE6F0',
+    },
+    {
+        code: 'latin',
+        bgColor: '#FFEAB9',
+    },
+    {
+        code: 'film_tv',
+        bgColor: '#FDDFD8',
+    },
+    {
+        code: 'country',
+        bgColor: '#bfffd3',
+    },
+    {
+        code: 'afro_beats',
+        bgColor: '#d7f8ff',
+    },
+    {
+        code: 'worldwide',
+        bgColor: '#FCE6F0',
+    },
+    {
+        code: 'REGGAE_DANCE_HALL',
+        bgColor: '#FFEAB9',
+    },
+    {
+        code: 'house',
+        bgColor: '#FDDFD8',
+    },
+    {
+        code: 'k_pop',
+        bgColor: '#bfffd3',
+    },
+    {
+        code: 'french_pop',
+        bgColor: '#d7f8ff',
+    },
+    {
+        code: 'SINGER_SONGWRITER',
+        bgColor: '#FCE6F0',
+    },
+    {
+        code: 'reg_mexico',
+        bgColor: '#FFEAB9',
+    },
+]
+
+export default genreCat
